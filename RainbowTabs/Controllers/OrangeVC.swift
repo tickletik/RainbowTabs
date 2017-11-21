987b8ef553bb3e2f62392ad53ff24f1ee4f3d9ec
Ronny Abraham <this.ronny@gmail.com>
2017-11-21 13:55:47 +0200

    add button and switch to orangeVC, also make generic segue to green vc

--- RainbowTabs/Controllers/OrangeVC.swift
+++ RainbowTabs/Controllers/OrangeVC.swift
@@ -21,7 +21,9 @@ class OrangeVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func orangeButton(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
